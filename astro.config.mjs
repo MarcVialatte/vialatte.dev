31422a010bff0472ef0d4356b692b8ba10c2b93e
Marc Vialatte <marc@vialatte.dev>
2025-01-17 16:18:18 +0100

    posts correction, now the layout is working; rework of the logos and flavicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 import tailwind from '@astrojs/tailwind';
+
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare(),
-  integrations: [tailwind()]
+  integrations: [tailwind(), mdx()]
 });
